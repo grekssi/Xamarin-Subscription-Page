0447abe0f79f7a166af07aec99819df68605d8d7
Bedros Garabedyan <bedo.bgroup@abv.bg>
2020-03-17 15:07:24 +0200

    Initial commit

---
+++ SubscriptionPlanSelector/SubscriptionPlanSelector/SubscriptionPlanSelector/Models/SubscriptionTypeModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubscriptionPlanSelector.Models
+{
+    public sealed class SubscriptionTypeModels
+    {
+        public string Name { get; set; }
+        public KeyValuePair<string, double>[] Properties { get; set; } 
+
+        public SubscriptionTypeModels(string name, KeyValuePair<string, double>[] properties)
+        {
+            Name = Name;
+            Properties = properties;
+        }
+    }
+}
---
+++ SubscriptionPlanSelector/SubscriptionPlanSelector/SubscriptionPlanSelector/Views/SubscriptionPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SubscriptionPlanSelector.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SubscriptionPlanSelector.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SubscriptionPage : ContentPage
+    {
+        async void FreeOptionInitializer(object sender, EventArgs args)
+        {
+            await freeFrame.ScaleTo(1.2, 50);
+            await freeFrame.ScaleTo(1, 50);
+            
+        }
+
+        public SubscriptionPage()
+        {
+            InitializeComponent();
+
+            this.BindingContext = new SubscriptionPageViewModel();
+
+            Initialize();
+        }
+
+        public async void Initialize()
+        {
+            await proFrame.FadeTo(1, 2000);
+            await basicFrame.FadeTo(1, 800);
+            await freeFrame.FadeTo(1, 800);
+        }
+
+    }
+}
